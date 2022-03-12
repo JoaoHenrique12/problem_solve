ec6576ff700063d47cb6023f45b62a624ee1bad7
Joao Henrique Paulino <joao.henrique1299@hotmail.com>
2022-03-12 10:10:14 -0300

    binary_search alterado

--- eda2/python/search_vector/binary_search.py
+++ eda2/python/search_vector/binary_search.py
@@ -1,9 +1,10 @@
 # lo = mid + 1 pois mid ja foi comparado.
 
-def binary_search(x:int,lst:list):
-    lo, up = 0,len(lst)-1
+
+def binary_search(x: int, lst: list):
+    lo, up = 0, len(lst) - 1
     while lo < up:
-        mid = (lo + up)//2
+        mid = (lo + up) // 2
         if x == lst[mid]:
             return mid
         elif x < lst[mid]:
@@ -11,6 +12,7 @@ def binary_search(x:int,lst:list):
         else:
             lo = mid + 1
     return -1
+
 
 nums = [1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7]
 
