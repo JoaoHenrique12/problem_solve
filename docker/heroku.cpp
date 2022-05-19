b692ade9c365c3bb66275bc239d1b0ffb1cdce8b
Joao Henrique Paulino <joao.henrique1299@hotmail.com>
2022-05-18 23:26:31 -0300

    heroku intro

---
+++ docker/heroku.cpp
@@ -0,0 +1,16 @@
+// Install Heroku
+sudo snap install heroku --classic
+heroku login
+// Dentro de um repositorio git
+heroku create
+
+git push heroku main
+heroku ps:scale web=1
+heroku open
+
+heroku logs --tail
+
+heroku local // Roda o app localmente.
+heroku run python manage.py shell
+
+.env // -> pasta de variaveis de ambiente.
