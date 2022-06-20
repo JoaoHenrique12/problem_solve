bc61956219cc786cd243b80fbc44cfc8cbecffe1
Joao Henrique <joao.henrique1299@hotmail.com>
2022-06-20 07:33:18 -0300

    python mini project

---
+++ docker/banco_python/python/main.py
@@ -0,0 +1,12 @@
+import psycopg2
+import time
+
+conn = psycopg2.connect(host="172.20.0.3",dbname="metalgear", user="postgres", password="postgres")
+cur = conn.cursor()
+while True:
+    cur.execute("select * from providers;")
+    obj = cur.fetchall()
+    for o in obj:
+        print(o)
+    time.sleep(10)
+conn.close()
