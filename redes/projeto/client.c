98626866b7b8dfe3555f4138dab8346381889b44
Joao Henrique <joao.henrique1299@hotmail.com>
2022-12-02 19:25:52 -0300

    Ajustando código para meu estilo de programação

--- redes/projeto/client.c
+++ redes/projeto/client.c
@@ -9,22 +9,21 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h> /* memset() */
+#include <string.h>
 #include <stdlib.h>
 
 #define MAX_MSG 100
 
 int main(int argc, char *argv[]) {
   int sd, rc, i;
-  struct sockaddr_in ladoCli;   /* dados do cliente local   */
-  struct sockaddr_in ladoServ; 	/* dados do servidor remoto */
+  struct sockaddr_in ladoCli;
+  struct sockaddr_in ladoServ;
   
 /* confere o numero de argumentos passados para o programa */
-  if(argc<3)  {
-    printf("uso correto: %s <ip_do_servidor> <porta_do_servidor> <dado1> ... <dadoN> \n", argv[0]);
-    exit(1);  }
+  if(argc<3)
+    { printf("uso correto: %s <ip_do_servidor> <porta_do_servidor> <dado1> ... <dadoN> \n", argv[0]); exit(1); }
 
-/* Preenchendo as informacoes de identificacao do remoto */
+/* Preenchendo as informacoes de identificacao do servidor */
   ladoServ.sin_family 	   = AF_INET;
   ladoServ.sin_addr.s_addr = inet_addr(argv[1]);
   ladoServ.sin_port 	   = htons(atoi(argv[2]));
@@ -38,26 +37,22 @@ int main(int argc, char *argv[]) {
 /* sd contem apenas dados sobre familia e protocolo  */
 
   sd = socket(AF_INET,SOCK_DGRAM,0);
-  if(sd<0) {
-    printf("%s: nao pode abrir o socket \n",argv[0]);
-    exit(1); }
+  if(sd<0) 
+    { printf("%s: nao pode abrir o socket \n",argv[0]); exit(1); }
 
 /* Relacionando o socket sd com a estrutura ladoCli */
 /* Depois do bind, sd faz referencia a protocolo local, ip local e porta local */
   rc = bind(sd, (struct sockaddr *) &ladoCli, sizeof(ladoCli));
-  if(rc<0) {
-    printf("%s: nao pode fazer um bind da porta\n", argv[0]);
-    exit(1); }
+  if(rc<0) 
+    { printf("%s: nao pode fazer um bind da porta\n", argv[0]); exit(1); }
   printf("{UDP, IP_Cli: %s, Porta_Cli: %u, IP_R: %s, Porta_R: %s}\n", inet_ntoa(ladoCli.sin_addr), ntohs(ladoCli.sin_port), argv[1], argv[2]);
 
   /* Enviando um pacote para cada parametro informado */
   for(i=3;i<argc;i++) {
     rc = sendto(sd, argv[i], strlen(argv[i]), 0,(struct sockaddr *) &ladoServ, sizeof(ladoServ));
-    if(rc<0) {
-      printf("%s: nao pode enviar dados %d \n",argv[0],i-1);
-      close(sd);
-      exit(1); }
+    if(rc<0) 
+      { printf("%s: nao pode enviar dados %d \n",argv[0],i-1); close(sd); exit(1); }
     printf("Enviando parametro %d: %s\n", i-2, argv[i]);
-  } /* fim do for (laco) */
-  return 1;
-} /* fim do programa */
+  }
+  return 0;
+}
