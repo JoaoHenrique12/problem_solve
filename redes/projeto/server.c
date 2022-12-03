cefd8153e90a702fd8edd8edb83a9422694247da
Joao Henrique <joao.henrique1299@hotmail.com>
2022-12-02 23:33:08 -0300

    Servidor 'modularizado'.

--- redes/projeto/server.c
+++ redes/projeto/server.c
@@ -14,43 +14,53 @@
 
 #define MAX_MSG 100
 
-int main(int argc, char *argv[]) {
-  int			sd, rc, n;
-  socklen_t		tam_Cli;
-  struct sockaddr_in	endCli;
-  struct sockaddr_in	endServ;
-  char			msg[MAX_MSG];
+int			sd, rc, n;
+socklen_t		tam_Cli;
+struct sockaddr_in	endCli;
+struct sockaddr_in	endServ;
+char			msg[MAX_MSG];
 
-  if (argc<3) 
-    { printf("Digite IP e Porta para este servidor\n"); exit(1); }
-/* Criacao do socket UDP */
+
+void start_server()
+{
   sd=socket(AF_INET, SOCK_DGRAM, 0);
   if(sd<0) 
-    { printf("%s: nao pode abrir o socket \n",argv[0]); exit(1);  }
+    { printf("Nao pode abrir o socket \n"); exit(1);  }
 
 /* Preenchendo informacoes sobre o servidor */
   endServ.sin_family 	  = AF_INET;
-  endServ.sin_addr.s_addr = inet_addr(argv[1]); 
-  endServ.sin_port 	  = htons(atoi(argv[2]));
+  endServ.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+  endServ.sin_port 	  = htons(3030);
 
 /* Fazendo um bind na porta local do servidor */
   rc = bind (sd, (struct sockaddr *) &endServ,sizeof(endServ));
   if(rc<0) 
-    { printf("%s: nao pode fazer bind na porta %s \n", argv[0], argv[2]); exit(1); }
+    { printf("Nao pode fazer bind na porta 3030 \n"); exit(1); }
 
-  printf("%s: esperando por dados no IP: %s, porta UDP numero: %s\n", argv[0], argv[1], argv[2]);
+  printf("Esperando por dados no IP: 127.0.0.1, porta UDP numero: 3030 \n");
 
-  while(1) {
+}
+
+void listen_server()
+{
+  memset(msg,0x0,MAX_MSG);
+  tam_Cli = sizeof(endCli);
+  
+  n = recvfrom(sd, msg, MAX_MSG, 0, (struct sockaddr *) &endCli, &tam_Cli);
+  if(n<0) 
+    { printf("Nao pode receber dados \n"); return;} 
     
-    memset(msg,0x0,MAX_MSG);
-    tam_Cli = sizeof(endCli);
-    
-    n = recvfrom(sd, msg, MAX_MSG, 0, (struct sockaddr *) &endCli, &tam_Cli);
-    if(n<0) 
-      { printf("%s: nao pode receber dados \n",argv[0]); continue;} 
-      
-    printf("{UDP, IP_L: %s, Porta_L: %u", inet_ntoa(endServ.sin_addr), ntohs(endServ.sin_port));
-    printf(" IP_R: %s, Porta_R: %u} => %s\n",inet_ntoa(endCli.sin_addr), ntohs(endCli.sin_port), msg);
-  } 
+  printf("{UDP, IP_L: %s, Porta_L: %u", inet_ntoa(endServ.sin_addr), ntohs(endServ.sin_port));
+  printf(" IP_R: %s, Porta_R: %u} => %s\n",inet_ntoa(endCli.sin_addr), ntohs(endCli.sin_port), msg);
+}
+
+int main() {
+  start_server();
+
+  while(1)
+  {
+    listen_server();
+  }
+
   return 0;
 }
