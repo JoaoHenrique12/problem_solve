7b9ea1988865396f50eb505cd05bfb085bf20ba0
Joao Henrique <joao.henrique1299@hotmail.com>
2023-06-04 18:27:12 -0300

    Decorators in python

---
+++ python/about_decorators.py
@@ -0,0 +1,60 @@
+"""
+Link: https://realpython.com/primer-on-python-decorators/
+
+High-Order Function is a function wich use or return another function.
+
+Functions in python are First class object, they can be passed as argumenst,
+stored in data structures, etc.
+
+Inner function, a function inside a fucntion.
+
+"""
+
+
+def part1():
+    def my_decorator(func):
+
+        def wrapper():
+            print("Something is happening before the function is called.")
+            func()
+            print("Something is happening after the function is called.")
+        return wrapper
+
+    def say_whee():
+        print("Whee!")
+
+    say_whee = my_decorator(say_whee)
+
+    say_whee()
+
+
+def part2():
+    def my_decorator(func):
+
+        def wrapper():
+            print("Something is happening before the function is called.")
+            func()
+            print("Something is happening after the function is called.")
+        return wrapper
+
+    @my_decorator
+    def say_whee():
+        print("Whee!")
+
+    say_whee()
+
+
+def part3():
+    def do_twice(func):
+        def wrapper_do_twice(*args, **kwargs):
+            func(*args, **kwargs)
+            return func(*args, **kwargs)
+        return wrapper_do_twice
+
+    @do_twice
+    def return_greeting(name):
+        print("Creating greeting")
+        return f"Hi {name}"
+
+    name = return_greeting("Sank")
+    print(name)
