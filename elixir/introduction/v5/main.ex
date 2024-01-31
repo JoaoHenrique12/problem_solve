ff3d1caf18807da53f670b897c213d0458a524ba
Joao Henrique <joao.henrique1299@hotmail.com>
2024-01-31 20:33:59 -0300

    Elixir, v5

--- elixir/introduction/v5/main.ex
+++ elixir/introduction/v5/main.ex
@@ -13,8 +13,20 @@ defmodule Playground do
     IO.puts("Time: #{delta}")
   end
 
-  defp fib(0), do: 0
-  defp fib(1), do: 1
   defp fib(n) when n < 0, do: :error
-  defp fib(n), do: fib(n - 1) + fib(n - 2)
+  defp fib(n), do: fib(n, 1, 0)
+  defp fib(0, _, result), do: result
+  defp fib(n, next, result), do: fib(n - 1, next + result, next)
 end
+
+# Enum.each([1,2,3], fn x -> IO.puts(x) end)
+# Enum.map([1,2,3], fn x -> x * 2 end)
+# Enum.filter([1,2,3], fn x -> Integer.is_odd(x) end)
+# Enum.reduce([1,2,3],0, fn x,y -> x + y end) # {iter}, {init}, {fn {acumulator}, {params}}
+
+# for x <- [1,2,3], y <- [1,2,3], do: {x,y, x*y}
+# for x <- 1..9, y <- 1..9, x < y, into: %{},  do: {{x,y}, x*y}
+
+# for x <- 1..9, y <- 1..9, x < y, into: %{} do
+#   {{x,y}, x*y}
+# end
