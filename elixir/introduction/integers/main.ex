79cddfb3db7a72f5bd3dd295b1ac8b521ddf61c9
Joao Henrique <joao.henrique1299@hotmail.com>
2024-02-05 17:23:17 -0300

    Elixir integers update.

--- elixir/introduction/integers/main.ex
+++ elixir/introduction/integers/main.ex
@@ -1,3 +1,4 @@
+# Float to integer.
 round(10.3)
 floor(10.3)
 trunc(10.3)
@@ -6,7 +7,13 @@ ceil(10.3)
 # Get line of integers
 IO.gets("") |> String.split() |> Enum.map(&String.to_integer/1)
 
-# Get mod, rem
-Integer.mod(10,3)
+# Operations
 rem(10,3)
+div 10, 3
+
+Integer.mod(10,3)
 Integer.pow(2,3)
+
+Integer.to_string 123
+
+:math.sin(:math.pi)
