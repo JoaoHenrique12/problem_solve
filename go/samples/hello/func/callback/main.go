25205a091ef12365b57741daf00463592301ff24
Joao Henrique <joao.henrique1299@hotmail.com>
2024-10-20 16:31:12 -0300

    feat(go): explanation callback function

--- go/samples/hello/func/callback/main.go
+++ go/samples/hello/func/callback/main.go
@@ -31,6 +31,11 @@ func remove_odd(f func(x... int)(int), values... int) int {
   return f(filtered...)
 }
 
+// A callback function is a function passed into another function as an argument,
+// which is then invoked inside the outer function to complete some kind of routine or action.
+// 
+// sum is the callback function in this case.
+
 func main() {
   a := makeRange(0,10)
   fmt.Println(a)
