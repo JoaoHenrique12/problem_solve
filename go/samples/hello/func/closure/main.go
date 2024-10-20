a9e7a7e459c7b0c63bf4b6b7881ececc9ad537b0
Joao Henrique <joao.henrique1299@hotmail.com>
2024-10-20 16:23:25 -0300

    feat(go): closure

--- go/samples/hello/func/closure/main.go
+++ go/samples/hello/func/closure/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+// Uma closure é a combinação de uma função com as referências ao estado que a circunda (o ambiente léxico).
+// Em outras palavras, uma closure lhe dá acesso ao escopo de uma função externa a partir de uma função interna.
+
 func closure() (func() int) {
   sum := 0
 
