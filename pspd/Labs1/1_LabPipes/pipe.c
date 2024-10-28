a76d2ea6ae90d5d07c226a510f29fc741322fe1b
Joao Henrique <joao.henrique1299@hotmail.com>
2024-10-28 15:14:46 -0300

    feat(pspd): add pipe file

---
+++ pspd/Labs1/1_LabPipes/pipe.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+  int fd[2], nbytes;
+  pid_t pid;
+  char string[] = "Olá, mundo!";
+  char readbuffer[80];
+
+  pipe(fd);
+  if ((pid = fork()) == -1) {
+    perror("fork");
+    exit(1);
+  }
+
+  if (pid == 0) {
+    /* Child process closes up input side of pipe */
+    close(fd[0]);
+    /* Send "string" through the output side of pipe */
+    printf("Processo [%d] --> Enviando string pelo pipe...\n", getpid());
+    write(fd[1], string, (strlen(string) + 1));
+    exit(0);
+  } else {
+    /* Parent process closes up output side of pipe */
+    close(fd[1]);
+    /* Read in a string from the pipe */
+    nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+    printf("Processo [%d] --> Recebendo string do pipe ...: %s\n", getpid(),
+           readbuffer);
+  }
+  return (0);
+}
