07bbcd571718d414b3cb6db82b65c765c76abb60
Joao Henrique <joao.henrique1299@hotmail.com>
2025-04-05 19:38:13 -0300

    chore(go): add explanation between fatal and panic

--- go/samples/errors/log/main.go
+++ go/samples/errors/log/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 )
-
+// Defers will be executed when a program panics, but calling os.Exit exits immediately, and deferred functions can't be run.
 func main()  {
   f, err := os.Create("log.txt")
   if err != nil {
@@ -16,7 +16,10 @@ func main()  {
 
   f2, err2 := os.Open("undefined_file.txt")
   if err2 != nil {
+    // execute OS.exit and get out
     // log.Fatalln(err2)
+
+    // Execute defers
     log.Panicln(err2)
   }
   var b []byte
