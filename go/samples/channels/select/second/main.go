6726eb91b4d15efd821ccd557bfeceba54c9e785
Joao Henrique <joao.henrique1299@hotmail.com>
2025-04-05 20:55:49 -0300

    refactor(go): second example of use select and channels in go
    select can be used to send information either

--- go/samples/channels/select/second/main.go
+++ go/samples/channels/select/second/main.go
@@ -6,18 +6,18 @@ func main() {
 	canal := make(chan int)
 	quit := make(chan int)
 
-	go getquit(canal, quit)
-	sendchannel(canal, quit)
+	go ask_for_consume_then_send_quit(canal, quit)
+	produce_until_receive_quit(canal, quit)
 }
 
-func getquit(canal, quit chan int) {
+func ask_for_consume_then_send_quit(canal, quit chan int) {
 	for i := 0; i < 50; i++ {
 		fmt.Println("recebido:", <-canal)
 	}
 	quit <- 0
 }
 
-func sendchannel(canal, quit chan int) {
+func produce_until_receive_quit(canal, quit chan int) {
 	dunno := 1
 	for {
 		select {
