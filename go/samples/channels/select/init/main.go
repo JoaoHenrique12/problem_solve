f4772e59b23c5e0ced067eaab06fbd8a57411b45
Joao Henrique <joao.henrique1299@hotmail.com>
2025-04-05 20:51:31 -0300

    chore(go): update explanation in init channel select example

--- go/samples/channels/select/init/main.go
+++ go/samples/channels/select/init/main.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-// Generally buffers are not used, they said
 
 func main() {
 
     var canal1 chan int = make(chan int)
     var canal2 chan int = make(chan int)
 
-    x := 500
+    x := 10
 
     go func (x int)  {
         for i := 0; i < x; i++{
@@ -24,6 +23,9 @@ func main() {
     }(x/2)
 
     for i := 0; i < x; i++ {
+		// The select statement allows the goroutine to wait on multiple
+		// communication operations. It will execute the first case that
+		// is ready to communicate.
         select {
         case v := <-canal1:
             fmt.Println("Canal A:", v)
